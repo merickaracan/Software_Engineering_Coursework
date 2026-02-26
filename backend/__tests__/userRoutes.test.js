19135027873c85022aef28274214e2def667bb6b
UsernameHere13 <aidan.robertson444@gmail.com>
2026-02-26 12:03:47 +0000

    Start of userRoutes testing

--- backend/__tests__/userRoutes.test.js
+++ backend/__tests__/userRoutes.test.js
@@ -1,3 +1,12 @@
+const test = require("node:test");
+const assert = require("node:assert");
 const request = require("supertest");
 const app = require("../app");
+const db = require("../db");
+
+jest.mock("../db");
+
+
+
+
 
